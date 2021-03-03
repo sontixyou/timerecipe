509b1746d05577e1d5ecfcd6a3e3d1e9ee29469b
kenken <angelbeatsviolin@gmail.com>
2021-03-03 10:24:46 +0900

    ユーザー画像　無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :recipes
+  # has_one_attached :image
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   with_options presence: true do
     validates :nickname
-    validates :image_attached
+    # validates :image_attached
     validates :email, uniqueness: { case_sensitive: true, message: 'はもう登録されています' },
                       format: { with: VALID_EMAIL_REGEX, message: '@マークがありません' }
 
@@ -16,12 +17,8 @@ class User < ApplicationRecord
     end
   end
 
-  has_many :recipes
-  has_one_attached :image
-
-
-  def image_attached
-    # binding.pry
-    errors.add(:image, :presence) if image.blank?
-  end
+  # def image_attached
+  #   binding.pry
+  #   errors.add(:image, :presence) if image.blank?
+  # end
 end
